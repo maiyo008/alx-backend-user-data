192bb9cc6b3d5e57232711fd45b25abbb1d70260
maiyo008 <tonymaiyo008@gmail.com>
2023-08-08 11:17:19 +0300

    Create Authentication class to manage API authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Manage API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Manage
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
