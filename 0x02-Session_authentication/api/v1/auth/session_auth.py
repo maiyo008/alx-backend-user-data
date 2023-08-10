d5f3bb02c8e27186239873a32d4df4d09dbed9b9
maiyo008 <tonymaiyo008@gmail.com>
2023-08-10 17:48:18 +0300

    Uodate SessioAuth class to create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,23 @@
 Manages session authentication
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Manage session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session id for a user_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        sessionID = uuid.uuid4()
+        self.user_id_by_session_id[user_id] = sessionID
+        return sessionID
