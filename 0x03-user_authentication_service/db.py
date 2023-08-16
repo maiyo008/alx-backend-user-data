ccf6c2ba733e460c3e8ce5485277cfdc23126af3
maiyo008 <tonymaiyo008@gmail.com>
2023-08-16 19:21:39 +0300

    Hot fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
-"""
-DB Module
-"""
-from sqlalchemy import create_engine
+"""Database module."""
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-
 
 from user import Base, User
 
 
 class DB:
-    """
-    DB class
-    """
+    """Database class."""
+
     def __init__(self) -> None:
-        """
-        Initialize a new DB instance
-        """
+        """Initialize a new DB instance."""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -27,19 +22,36 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """
-        Memoized session object
-        """
+        """Memoized session object."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Add user
-        """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        """Adds a new user to the database."""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
+        return new_user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters."""
+        if not kwargs:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id."""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+
+        self._session.commit()
+        return None
