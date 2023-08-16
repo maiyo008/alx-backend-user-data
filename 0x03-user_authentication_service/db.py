b8442e414ca781d203fcaa7c2cb63133a5747d78
maiyo008 <tonymaiyo008@gmail.com>
2023-08-16 19:59:35 +0300

    Hot fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -56,7 +56,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: str, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """
         Update user
         """
