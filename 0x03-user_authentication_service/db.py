d182d4eb7d4c306629eacc17988bfdbb6a22a61b
maiyo008 <tonymaiyo008@gmail.com>
2023-08-16 19:36:28 +0300

    Hot fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
-"""Database module."""
-from sqlalchemy import create_engine, tuple_
-from sqlalchemy.exc import InvalidRequestError
+"""
+DB Module
+"""
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 
 from user import Base, User
 
 
 class DB:
-    """Database class."""
-
+    """
+    DB class
+    """
     def __init__(self) -> None:
-        """Initialize a new DB instance."""
+        """
+        Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -22,36 +28,55 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object."""
+        """
+        Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database."""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """
+        Add user
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
-    
-    def find_user_by(self, **kwargs) -> User:
-        """Finds a user based on a set of filters."""
-        if not kwargs:
-            raise InvalidRequestError
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if not user:
-            raise NoResultFound
         return user
-    
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """Updates a user based on a given id."""
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError
-            setattr(user, key, value)
 
-        self._session.commit()
-        return None
+    def find_user_by(self, **kwargs):
+        """
+        Find user
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found for the given criteria")
+            return user
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as e:
+            raise e
+
+    def update_user(self, user_id: str, **kwargs) -> None:
+        """
+        Update user
+        """
+        allowed_attr = [
+            'email',
+            'hashed_password',
+            'session_id',
+            'reset_token'
+        ]
+        try:
+            user = self.find_user_by(id=user_id)
+            for attr, value in kwargs.items():
+                if attr not in allowed_attr:
+                    raise ValueError("Invalid attribute: {}", attr)
+                setattr(user, attr, value)
+            self._session.commit()
+        except NoResultFound:
+            return None
+        except InvalidRequestError:
+            self._session.rollback()
