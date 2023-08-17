d3e1d11ca607bb413d3024f566d0550998782a2a
maiyo008 <tonymaiyo008@gmail.com>
2023-08-17 22:55:57 +0300

    generate reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,3 +92,19 @@ class Auth:
             return None
         except ValueError:
             return
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generate reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is None:
+                raise ValueError
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            return
+        except ValueError:
+            return
